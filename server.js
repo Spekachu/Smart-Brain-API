2c790c21378060736c1496a3ac8d9e4f7e4774b5
Spencer Johnson <Spekachu@users.noreply.github.com>
2018-05-23 18:26:11 -0700

    revert

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const path = require('path');
 const knex = require('knex')({
     client: 'pg',
     connection: {
